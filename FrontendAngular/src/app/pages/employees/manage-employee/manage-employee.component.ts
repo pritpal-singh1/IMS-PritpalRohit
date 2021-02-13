d0ca8977c2438dd7c13b3be8b3fb60a600e4f5f7
pritpal-singh1 <pritpaldabb@gmail.com>
2021-02-13 21:02:08 +0530

    datatable added in manageemployee

--- FrontendAngular/src/app/pages/employees/manage-employee/manage-employee.component.ts
+++ FrontendAngular/src/app/pages/employees/manage-employee/manage-employee.component.ts
@@ -1,6 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
 
+class DataTablesResponse {
+  data: any[];
+  draw: number;
+  recordsFiltered: number;
+  recordsTotal: number;
+}
+
 @Component({
   selector: 'app-manage-employee',
   templateUrl: './manage-employee.component.html',
@@ -9,12 +16,34 @@ import {Router} from '@angular/router';
 export class ManageEmployeeComponent implements OnInit {
 
   breadCrumbItems: Array<{}>;
-
+  // dtOptions: DataTables.Settings = {};
+  // persons: Person[];
   constructor(private router:Router) { }
 
   ngOnInit(): void {
     this.breadCrumbItems = [{ label: 'Employees' }, { label: 'Manage Employee', active: true }];
+    // this.dtOptions = {
+    //   pagingType: 'full_numbers',
+    //   pageLength: 2,
+    //   serverSide: true,
+    //   processing: true,
+    //   ajax: (dataTablesParameters: any, callback) => {
+    //     this.http
+    //       .post<DataTablesResponse>(
+    //         'https://angular-datatables-demo-server.herokuapp.com/',
+    //         dataTablesParameters, {}
+    //       ).subscribe(resp => {
+    //         this.persons = resp.data;
 
+    //         callback({
+    //           recordsTotal: resp.recordsTotal,
+    //           recordsFiltered: resp.recordsFiltered,
+    //           data: []
+    //         });
+    //       });
+    //   },
+    //   columns: [{ data: 'id' }, { data: 'firstName' }, { data: 'lastName' }]
+    // };
   }
   addEmployee(){
     this.router.navigate(['/employees/add-employee']);
